c200d3c43de6b819651cda3c2e7ab99f8f6ae7b8
Lucas Bui <hoangbui674@gmail.com>
2022-06-07 17:21:37 +0700

    Them code

---
+++ DoAnMMH_DigitalWatermarking/DoAn/WatermarkingDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+
+[assembly: AssemblyTitle("WatermarkingDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WatermarkingDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2022 UIT")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+
+[assembly: ComVisible(false)]
+
+
+
+
+[assembly: ThemeInfo(
+    ResourceDictionaryLocation.None, 
+    ResourceDictionaryLocation.SourceAssembly 
+)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
